f354b43682d36c05d2891f7b0c9ef42de7bafb29
Cristian Mendoza <cristian8mendoza8@gmail.com>
2025-10-20 22:28:23 -0600

    Paso final

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl: 'http://192.168.110.89:4000/api',
   // alias opcional por si algún archivo viejo usa apiBase
-  apiBase: 'http://localhost:4000/api'
+  apiBase: 'http://192.168.110.89:4000/api'
 };
